dd20ba96ffcc963573d97af3ed684756464bb21f
captbilard <bilardosinulu@gmail.com>
2019-12-11 08:35:23 +0100

    Set up template for voting app

--- SLAW/slaw/urls.py
+++ SLAW/slaw/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('nominees/', views.NomineesView.as_view(), name="nominees"),
+    path('vote/', views.VotingCategory.as_view(), name="voting-category"),
+
     
 ]
--- SLAW/slaw/views.py
+++ SLAW/slaw/views.py
@@ -1,12 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.views import generic
 from .models import Nominees, Votes, awardCategories
 import datetime
 
+
+categories = awardCategories.objects.all()
+categories_context = {'categories':categories}
 def index(request):
-    categories = awardCategories.objects.all()
-    return render(request, "slaw/index.html", {'categories':categories})
+    return render(request, "slaw/index.html", context=categories_context)
 
 
 
@@ -15,6 +17,14 @@ class NomineesView(generic.ListView):
     template_name = "slaw/nominees.html"
     context_object_name = 'nominee_list'
 
-    
+class VotingCategory(generic.ListView):
+    model = awardCategories
+    template_name = "slaw/voting-category.html"
+    context_object_name = 'categories'
+    # categories = awardCategories.objects.all()
+    # return render(request, "slaw/voting-category.html", {'categories':categories})
         
-# def vote
+# def voting(request, category_id):
+#     category = get_object_or_404(awardCategories, pk=category_id)
+#     return render(request, "slaw/nominee-list.html", {'category':category_id})
+
