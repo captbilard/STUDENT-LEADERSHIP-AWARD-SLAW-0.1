ad5fcefff876ce3008bae5e17538d091fbe5e9d8
captbilard <bilardosinulu@gmail.com>
2019-12-15 01:08:22 +0100

    implemented voting feature

--- SLAW/slaw/urls.py
+++ SLAW/slaw/urls.py
@@ -4,7 +4,9 @@ from . import views
 urlpatterns = [
     path('', views.index, name="index"),
     path('nominees/', views.NomineesView.as_view(), name="nominees"),
-    path('vote/', views.VotingCategory.as_view(), name="voting-category"),
+    path('vote-category/', views.VotingCategory.as_view(), name="voting-category"),
+    path('vote-category/<int:category_id>/', views.votingList, name="voting-list"),
+    path('<int:nominee_id>/vote/', views.vote, name="vote")
 
     
 ]
--- SLAW/slaw/views.py
+++ SLAW/slaw/views.py
@@ -2,14 +2,15 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
 from django.views import generic
 from .models import Nominees, Votes, awardCategories
+from django.urls import reverse
 import datetime
 
 
 categories = awardCategories.objects.all()
 categories_context = {'categories':categories}
+
 def index(request):
     return render(request, "slaw/index.html", context=categories_context)
-
 
 
 class NomineesView(generic.ListView):
@@ -24,7 +25,27 @@ class VotingCategory(generic.ListView):
     # categories = awardCategories.objects.all()
     # return render(request, "slaw/voting-category.html", {'categories':categories})
         
-# def voting(request, category_id):
-#     category = get_object_or_404(awardCategories, pk=category_id)
-#     return render(request, "slaw/nominee-list.html", {'category':category_id})
+def votingList(request, category_id):
+    category = get_object_or_404(awardCategories, pk=category_id)
+    return render(request, "slaw/voting-list.html", {'category':category})
+
+def vote(request, nominee_id):
+    nominee = get_object_or_404(Nominees, pk=nominee_id)
+    # print(nominee_id)
+    # selected_nominee = nominee.votes_set.get(pk=nominee_id -1)
+    # print(selected_nominee)
+    # return HttpResponseRedirect(reverse('index'))
+    try:
+        selected_nominee = nominee.votes_set.get(pk=nominee_id - 1)
+    except (KeyError, Votes.DoesNotExist):
+        return render(request, 'slaw/voting-list.html', {'error_message':"You didn't select a nominee"})
+    else:
+        print(selected_nominee)
+        selected_nominee.number_of_votes += 1
+        selected_nominee.save()
+        return HttpResponseRedirect(reverse('index'))
+    
+
+
+ 
 
